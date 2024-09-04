6367caafa0845bf655748705d050079ebd2e702f
Without-tension <77rostik33@gmail.com>
2024-09-04 20:03:25 +0300

    o

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: https://mutual-coins.com".$_SERVER['REQUEST_URI']);
+header("Location: http://mutual-coins.com".$_SERVER['REQUEST_URI']);
 exit();
 ?>
